c0cbf9b456d95bb4ce6e30ca0462fdc27ce98db3
Gustavo Carvalho Vaz <gut_ogut@hotmail.com>
2025-02-17 09:29:51 -0300

    Exercício de Classes

---
+++ ExercicioClasse1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Exercicios
+{
+
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+
+    class Princial
+    {
+
+        static void Main(string[] args)
+        {
+
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Console.WriteLine("Digite seus dados:");
+            Console.Write("nome:");
+            p1.Nome = Console.ReadLine();
+            Console.Write("idade:");
+            p1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os dados da outra pessoa:");
+            Console.Write("nome:");
+            p2.Nome = Console.ReadLine();
+            Console.Write("idade:");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine(" Pessoa mais velha:" + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine(" Pessoa mais velha:" + p2.Nome);
+            }
+        }
+    }
+}
